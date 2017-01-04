397b1724acffd4b2467ea22cea19cbecd80b0ee3
qbergen <queenabergen@gmail.com>
2017-01-03 19:51:14 -0500

    Second Commit

--- app/src/main/java/com/example/queenabergen/testremodeled/MainActivity.java
+++ app/src/main/java/com/example/queenabergen/testremodeled/MainActivity.java
@@ -2,8 +2,17 @@ package com.example.queenabergen.testremodeled;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = " ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +20,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://jsjrobotics.nyc/cgi-bin/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        APIService apiService = retrofit.create(APIService.class);
+        Call<Example> call = apiService.getAPIinfo();
+        call.enqueue(new Callback<Example>() {
+            @Override
+            public void onResponse(Call<Example> call, Response<Example> response) {
+                if (response.isSuccessful()) {
+                    Log.d(TAG, "Success :" + response.body());
+                } else {
+                    Log.d(TAG, "Error: Something Happened" + response.body().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Example> call, Throwable t) {
+
+
+            }
+        });
     }
 
 
